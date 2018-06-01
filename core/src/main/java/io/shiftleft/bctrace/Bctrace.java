8bff9c5a5a4e96c75a6ce89cda1edf89c76abb24
idelvall <idelvall@brutusin.org>
2018-06-01 14:52:06 +0200

    remove unused methods

--- core/src/main/java/io/shiftleft/bctrace/Bctrace.java
+++ core/src/main/java/io/shiftleft/bctrace/Bctrace.java
@@ -29,19 +29,13 @@ import io.shiftleft.bctrace.debug.CallCounterHook;
 import io.shiftleft.bctrace.debug.DebugInfo;
 import io.shiftleft.bctrace.impl.InstrumentationImpl;
 import io.shiftleft.bctrace.logging.Level;
+import io.shiftleft.bctrace.logging.Logger;
 import io.shiftleft.bctrace.runtime.Callback;
 import io.shiftleft.bctrace.runtime.listener.Listener;
 import io.shiftleft.bctrace.spi.AgentLoggerFactory;
 import io.shiftleft.bctrace.spi.Hook;
 import io.shiftleft.bctrace.spi.Instrumentation;
-import io.shiftleft.bctrace.logging.Logger;
 import io.shiftleft.bctrace.spi.SystemProperty;
-import java.io.IOException;
-import java.net.JarURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.security.ProtectionDomain;
 
 /**
  * Framework entry point.
@@ -125,50 +119,6 @@ public final class Bctrace {
 
   public Hook[] getHooks() {
     return this.hooks;
-  }
-
-  public static URL getCodeSource(String className, ProtectionDomain protectionDomain,
-      ClassLoader cl) {
-    if (protectionDomain != null && protectionDomain.getCodeSource() != null) {
-      return protectionDomain.getCodeSource().getLocation();
-    }
-    if (cl == null) {
-      return null;
-    }
-    String resourceName = className.replace('.', '/') + ".class";
-    URL url = cl.getResource(className.replace('.', '/') + ".class");
-    return getCodeSource(resourceName, url);
-  }
-
-  public static URL getCodeSource(Class clazz) {
-    return getCodeSource(clazz.getName(), clazz.getProtectionDomain(), clazz.getClassLoader());
-  }
-
-  public static URL getCodeSource(String resourceName, URL resourceUrl) {
-    if (resourceUrl == null) {
-      return null;
-    }
-    URLConnection urlConnection;
-    try {
-      urlConnection = resourceUrl.openConnection();
-    } catch (IOException ex) {
-      throw new RuntimeException(ex);
-    }
-    if (urlConnection instanceof JarURLConnection) {
-      JarURLConnection jarURLConnection = (JarURLConnection) urlConnection;
-      return jarURLConnection.getJarFileURL();
-    }
-    String str = resourceUrl.toString().replace(resourceName, "");
-    if (str.endsWith("!/")) {
-      str = str.substring(0, str.length() - 2);
-    } else if (str.endsWith("!")) {
-      str = str.substring(0, str.length() - 1);
-    }
-    try {
-      return new URL(str);
-    } catch (MalformedURLException ex) {
-      throw new AssertionError();
-    }
   }
 
   public static Logger getAgentLogger() {
