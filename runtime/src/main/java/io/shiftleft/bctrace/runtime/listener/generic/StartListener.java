445b6ce0496d23d7aee02ef11d096e831b030f3b
nacho <idelvall@brutusin.org>
2019-03-15 18:10:15 +0100

    add

---
+++ runtime/src/main/java/io/shiftleft/bctrace/runtime/listener/generic/StartListener.java
@@ -0,0 +1,45 @@
+/*
+ * ShiftLeft, Inc. CONFIDENTIAL
+ * Unpublished Copyright (c) 2017 ShiftLeft, Inc., All Rights Reserved.
+ *
+ * NOTICE: All information contained herein is, and remains the property of ShiftLeft, Inc.
+ * The intellectual and technical concepts contained herein are proprietary to ShiftLeft, Inc.
+ * and may be covered by U.S. and Foreign Patents, patents in process, and are protected by
+ * trade secret or copyright law. Dissemination of this information or reproduction of this
+ * material is strictly forbidden unless prior written permission is obtained
+ * from ShiftLeft, Inc. Access to the source code contained herein is hereby forbidden to
+ * anyone except current ShiftLeft, Inc. employees, managers or contractors who have executed
+ * Confidentiality and Non-disclosure agreements explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication or disclosure
+ * of this source code, which includes information that is confidential and/or proprietary, and
+ * is a trade secret, of ShiftLeft, Inc.
+ *
+ * ANY REPRODUCTION, MODIFICATION, DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC DISPLAY
+ * OF OR THROUGH USE OF THIS SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF ShiftLeft, Inc.
+ * IS STRICTLY PROHIBITED, AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES.
+ * THE RECEIPT OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT
+ * CONVEY OR IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS
+ * CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+ */
+package io.shiftleft.bctrace.runtime.listener.generic;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author Ignacio del Valle Alles idelvall@shiftleft.io
+ */
+public abstract class StartListener extends GenericListener {
+
+  /**
+   * Invoked by instrumented methods before any of its original instructions (if multiple plugins
+   * are registered, listener notification is performed according to their respective plugin
+   * registration order).
+   *
+   * @param methodId method id (as defined by MethodRegistry)
+   * @param clazz class defining the method.
+   * @param instance instance where the method is invoked. Null if the method is static
+   * @param args arguments passed to the method. returns false;
+   */
+  public abstract void onStart(int methodId, Class clazz, Object instance, Object[] args);
+}
